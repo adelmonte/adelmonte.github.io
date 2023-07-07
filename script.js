43dab515c4a862336c2aad68ad2db3c4ea9cf64b
adelmonte <138840776+adelmonte@users.noreply.github.com>
2023-07-06 23:13:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,135 +1,108 @@
-window.human = false;
+let canvas = document.querySelector("canvas")
 
-var canvasEl = document.querySelector('.fireworks');
-var ctx = canvasEl.getContext('2d');
-var numberOfParticules = 30;
-var pointerX = 0;
-var pointerY = 0;
-var tap = ('ontouchstart' in window || navigator.msMaxTouchPoints) ? 'touchstart' : 'mousedown';
-var colors = ['#FF1461', '#18FF92', '#5A87FF', '#FBF38C'];
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
 
-function setCanvasSize() {
-  canvasEl.width = window.innerWidth * 2;
-  canvasEl.height = window.innerHeight * 2;
-  canvasEl.style.width = window.innerWidth + 'px';
-  canvasEl.style.height = window.innerHeight + 'px';
-  canvasEl.getContext('2d').scale(2, 2);
+let ctx = canvas.getContext('2d')
+ 
+ let colors =[
+  '#FFFF00',
+  '#e74c3c',
+  '#ecf0f1',
+  '#349b0b',
+  '#2980b9',
+ ]
+
+ let mouse ={
+  x:undefined,
+  y:undefined,
+ }
+
+ let gravity = 1
+ let friction = .98
+
+ document.addEventListener("mousemove",(e)=>{
+  mouse.x=e.x
+  mouse.y=e.y
+ })
+
+ window.addEventListener("resize",()=>{
+  canvas.width=window.innerWidth
+  canvas.height=window.innerHeight
+  init()
+ })
+
+ window.addEventListener('click',()=>{
+  init()
+ })
+
+ // Utility Functions
+function randomIntFromRange(min,max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function updateCoords(e) {
-  pointerX = e.clientX || e.touches[0].clientX;
-  pointerY = e.clientY || e.touches[0].clientY;
+function randomColor(colors) {
+	return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function setParticuleDirection(p) {
-  var angle = anime.random(0, 360) * Math.PI / 180;
-  var value = anime.random(50, 180);
-  var radius = [-1, 1][anime.random(0, 1)] * value;
-  return {
-    x: p.x + radius * Math.cos(angle),
-    y: p.y + radius * Math.sin(angle)
+ function Ball(x,y,dy,dx,r,color){
+ this.x=x
+ this.y=y
+ this.dy=dy
+ this.dx=dx
+ this.r=r
+ this.color=color
+
+ this.Draw = function(){
+  ctx.beginPath()
+  ctx.arc(this.x,this.y,this.r,0,Math.PI*2 ,false)
+  ctx.strokeStyle="blue"
+  ctx.stroke()
+  ctx.fillStyle=this.color
+  ctx.fill()
+ }
+
+ this.Update = function(){
+  if(this.y+this.r>canvas.height){
+   this.dy=-this.dy*friction
+  }
+  else{
+   this.dy+=gravity
+  }
+  if(this.x+this.r>canvas.width||this.x-this.r<=0){
+   this.dx=-this.dx
+  }
+  this.y+=this.dy
+  this.x+=this.dx
+  this.Draw()
+ }
+ }
+
+ let ball
+ let ballArray
+function init(){
+ ballArray=[]
+  for(let i=0; i<400;i++){
+   let raduis=randomIntFromRange(8,20)
+   let x=randomIntFromRange(0,canvas.width-raduis)
+   let y=randomIntFromRange(0,canvas.height-raduis)
+   let dx =randomIntFromRange(-2,2)
+   let dy =randomIntFromRange(-2,2)
+   let color = randomColor(colors)
+   ballArray.push(new Ball(x,y,dy,dx,raduis,color))
   }
 }
 
-function createParticule(x,y) {
-  var p = {};
-  p.x = x;
-  p.y = y;
-  p.color = colors[anime.random(0, colors.length - 1)];
-  p.radius = anime.random(16, 32);
-  p.endPos = setParticuleDirection(p);
-  p.draw = function() {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-    ctx.fillStyle = p.color;
-    ctx.fill();
+init()
+
+
+ function animate(){
+  requestAnimationFrame(animate)
+  ctx.clearRect(0,0,window.innerWidth,window.innerHeight)
+  for(let i =0 ;i<ballArray.length;i++){
+   ballArray[i].Update()
   }
-  return p;
-}
+  // canvas.fillText("HTML Canvas PoilerBlate" ,mouse.x,mouse.y)
+ }
 
-function createCircle(x,y) {
-  var p = {};
-  p.x = x;
-  p.y = y;
-  p.color = '#FFF';
-  p.radius = 0.1;
-  p.alpha = .5;
-  p.lineWidth = 6;
-  p.draw = function() {
-    ctx.globalAlpha = p.alpha;
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
-    ctx.lineWidth = p.lineWidth;
-    ctx.strokeStyle = p.color;
-    ctx.stroke();
-    ctx.globalAlpha = 1;
-  }
-  return p;
-}
-
-function renderParticule(anim) {
-  for (var i = 0; i < anim.animatables.length; i++) {
-    anim.animatables[i].target.draw();
-  }
-}
-
-function animateParticules(x, y) {
-  var circle = createCircle(x, y);
-  var particules = [];
-  for (var i = 0; i < numberOfParticules; i++) {
-    particules.push(createParticule(x, y));
-  }
-  anime.timeline().add({
-    targets: particules,
-    x: function(p) { return p.endPos.x; },
-    y: function(p) { return p.endPos.y; },
-    radius: 0.1,
-    duration: anime.random(1200, 1800),
-    easing: 'easeOutExpo',
-    update: renderParticule
-  })
-    .add({
-    targets: circle,
-    radius: anime.random(80, 160),
-    lineWidth: 0,
-    alpha: {
-      value: 0,
-      easing: 'linear',
-      duration: anime.random(600, 800),  
-    },
-    duration: anime.random(1200, 1800),
-    easing: 'easeOutExpo',
-    update: renderParticule,
-    offset: 0
-  });
-}
-
-var render = anime({
-  duration: Infinity,
-  update: function() {
-    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-  }
-});
-
-document.addEventListener(tap, function(e) {
-  window.human = true;
-  render.play();
-  updateCoords(e);
-  animateParticules(pointerX, pointerY);
-}, false);
-
-var centerX = window.innerWidth / 2;
-var centerY = window.innerHeight / 2;
-
-function autoClick() {
-  if (window.human) return;
-  animateParticules(
-    anime.random(centerX-50, centerX+50), 
-    anime.random(centerY-50, centerY+50)
-  );
-  anime({duration: 200}).finished.then(autoClick);
-}
-
-autoClick();
-setCanvasSize();
-window.addEventListener('resize', setCanvasSize, false);
+ animate()
